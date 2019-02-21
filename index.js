dbb60ff4e4a144ce040531d9d10cd1d6801df9e5
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-21 18:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-// Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-    return drivers.slice(0, 2);
-  };
+  return drivers.slice(0,2);
+};
 
-  const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-  };
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(drivers.length-2,drivers.lenght);
+};
 
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-  const createFareMultiplier = function (x) {
-    return function (value) {
-      return x * value;
-    };
-  };
+const createFareMultiplier = function (multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  }
+}
 
-  const fareDoubler = createFareMultiplier(2);
+const fareDoubler = createFareMultiplier(2);
 
-  const fareTripler = createFareMultiplier(3);
+const fareTripler = createFareMultiplier(3);
 
-  const DifferentDrivers = function(drivers, selection) {
-    return selection(drivers);
-  };
+const fetchSpecifiedDrivers = function (drivers, fn) {
+  return fn(drivers);
+}
