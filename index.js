05cf1223ce5839754fdcbdbde70cd82726eeb0d8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-21 18:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const returnFirstTwoDrivers = function (drivers) {
   const returnLastTwoDrivers = function(drivers) {
     return drivers.slice(-2);
   };
-  
+
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
